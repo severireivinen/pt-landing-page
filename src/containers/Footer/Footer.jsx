4b29e3022ce6f6953b9bf4db72f97d9f9bf37372
Severi Reivinen <severire@metropolia.fi>
2022-03-03 19:26:11 +0200

    fixed production warning

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     email: "",
     message: "",
   });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  //const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const { name, email, message } = formData;
